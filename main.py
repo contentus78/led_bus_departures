0dfa6aa6fa2aca4ae08582ba06fcfff990e4cc4e
contentus78 <leon.baumeister@protonmail.com>
2024-06-10 23:23:09 +0200

    added sun

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ bus_lines = [
 
 white = graphics.Color(255, 255, 255)
 sample_time = "22:35"
-sample_temperature = "17°C" 
+sample_temperature = "17°C"
+
+yellow = graphics.Color(255, 255, 0)
+
 
 def display_bus_times(bus_lines):
     # Sort bus lines based on the next departure time
@@ -40,9 +43,27 @@ def display_bus_times(bus_lines):
         # Adjust the y position calculation based on the new font height
         graphics.DrawText(matrix, font, 2, 7 + i * 8, color, text)
 
+    # Sun part
     # Display additional information
     graphics.DrawText(matrix, font, 40, 7, white, sample_time)
     graphics.DrawText(matrix, font, 40, 15, white, sample_temperature)
+
+    core_start_x, core_start_y = 40, 17
+    graphics.DrawRect(matrix, core_start_x, core_start_y, 6, 6, yellow)
+
+    # Draw rays extending from the sun core
+    # Horizontal and vertical rays
+    graphics.DrawLine(matrix, 1, 7, 6, 7, yellow)
+    graphics.DrawLine(matrix, 13, 7, 18, 7, yellow)
+    graphics.DrawLine(matrix, 9, 1, 9, 3, yellow)
+    graphics.DrawLine(matrix, 9, 10, 9, 14, yellow)
+
+    # Diagonal rays
+    graphics.DrawLine(matrix, 4, 4, 1, 1, yellow)
+    graphics.DrawLine(matrix, 4, 10, 1, 13, yellow)
+    graphics.DrawLine(matrix, 14, 4, 17, 1, yellow)
+    graphics.DrawLine(matrix, 14, 10, 17, 13, yellow)
+
 
 try:
     while True:
